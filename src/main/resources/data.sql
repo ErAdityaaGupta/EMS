5ac845e6217120b36cdc6dc8d38646fcaac75ceb
ErAdityaaGupta <159624433+ErAdityaaGupta@users.noreply.github.com>
2025-02-27 19:49:34 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO employees (name, city) VALUES ('John Doe', 'New York');
 INSERT INTO employees (name, city) VALUES ('Jane Smith', 'Los Angeles');
 INSERT INTO employees (name, city) VALUES ('Michael Johnson', 'Chicago');
+INSERT INTO employees (name, city) VALUES ('Aditya Gupta', 'Hisar');
